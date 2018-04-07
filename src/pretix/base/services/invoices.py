e2131fa67e4de365eee150642b194ff9929591b9
Raphael Michel <mail@raphaelmichel.de>
2018-04-07 21:30:40 +0200

    Add new shredders and download confirmation

--- src/pretix/base/services/invoices.py
+++ src/pretix/base/services/invoices.py
@@ -178,6 +178,8 @@ def generate_cancellation(invoice: Invoice, trigger_pdf=True):
 
 
 def regenerate_invoice(invoice: Invoice):
+    if invoice.shredded:
+        return invoice
     if invoice.is_cancellation:
         invoice = build_cancellation(invoice)
     else:
@@ -212,6 +214,10 @@ def generate_invoice(order: Order, trigger_pdf=True):
 @app.task(base=TransactionAwareTask)
 def invoice_pdf_task(invoice: int):
     i = Invoice.objects.get(pk=invoice)
+    if i.shredded:
+        return None
+    if i.file:
+        i.file.delete()
     with language(i.locale):
         fname, ftype, fcontent = i.event.invoice_renderer.generate(i)
         i.file.save(fname, ContentFile(fcontent))
